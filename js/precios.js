077d51f0a448585c9e8c71cd465a33abc4528f12
Dullahhan <130026493+Dullahhan@users.noreply.github.com>
2025-03-03 10:00:14 -0500

    Update precios.js

--- js/precios.js
+++ js/precios.js
@@ -1,1 +1,21 @@
-
+// precios.js
+const pizzaPrecios = {
+    // Pizza de Queso
+    "Pizza de Queso": "120 cup",
+    // Pizza Doble Queso
+    "Pizza Doble Queso": "180 cup",
+    // Pizza Vegetariana
+    "Pizza Vegetariana": "180 cup",
+    // Pizza de Queso y Salchicha
+    "Pizza de Queso y Salchicha": "250 cup",
+    // Pizza Hawaiana
+    "Pizza Hawaiana": "350 cup",
+    // Pizza de pollo
+    "Pizza de Pollo": "500 cup",
+    // Pizza de Jamón
+    "Pizza de Jamon": "1000 cup",
+    // Pizza Especial
+    "Pizza Especial": "1250 cup",
+    // Especial de la Casa
+    "especial": "$17.00"
+};
